862c050950d7a151cf23c2456f27e7bab032536e
Patrick Fuller <patrickfuller@Patricks-MacBook-Pro.local>
2022-03-21 10:45:25 -0400

    Add start of project

---
+++ happy-tracker/happy_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  happy_trackerApp.swift
+//  happy-tracker
+//
+//  Created by Patrick Fuller on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct happy_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
