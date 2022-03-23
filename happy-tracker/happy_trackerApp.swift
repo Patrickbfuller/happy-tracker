99ac239a8529b4a695fa78b32a678c860b98b9b8
Patrick Fuller <patrickfuller@Patricks-MacBook-Pro.local>
2022-03-23 15:14:33 -0400

    Add firebase config

--- happy-tracker/happy_trackerApp.swift
+++ happy-tracker/happy_trackerApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct happy_trackerApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
           //  ContentView()
