f53c1c67b53e1937e4612c58fc5841e9eec9c1eb
Patrick Fuller <patrickfuller@Patricks-MacBook-Pro.local>
2022-03-30 13:23:17 -0400

    Extend codable w toDict() firebase upload

--- happy-tracker/Util/Codable+Ext.swift
+++ happy-tracker/Util/Codable+Ext.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension Encodable {
+    func toDict() -> [String: Any]? {
+        var dict: [String: Any]? = nil
+        do {
+            let data = try JSONEncoder().encode(self)
+            dict = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any]
+        } catch {
+            print(error)
+        }
+        return dict
+    }
+}
