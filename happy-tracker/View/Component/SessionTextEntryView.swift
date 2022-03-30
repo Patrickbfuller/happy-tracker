7b6a4904ec4e2e65397b679eef3b71da09716982
Patrick Fuller <patrickfuller@Patricks-MacBook-Pro.local>
2022-03-30 13:29:42 -0400

    Fix text appearance in darkmode

--- happy-tracker/View/Component/SessionTextEntryView.swift
+++ happy-tracker/View/Component/SessionTextEntryView.swift
@@ -18,6 +18,13 @@ struct SessionTextEntryView: View {
                 .ignoresSafeArea()
             VStack(spacing: 0) {
                 
+                ZStack {
+                    TextEditor(text: $description)
+                        .padding(8)
+                        .foregroundColor(Color.primary)
+                        .background(Color(UIColor.systemBackground))
+                }
+                
                 HStack {
                     Button { // action
                         sessionViewModel.resetSession()
@@ -25,31 +32,24 @@ struct SessionTextEntryView: View {
                         Text("Cancel Entry")
                             .padding()
                     }
-                    Spacer()
-                }
-                
-                TextEditor(text: $description)
-                    .padding(8)
-                    .foregroundColor(.black)
-                    .background(.white)
-                
-                Button { // action
-                    sessionViewModel.submitSession(comment: description)
-                } label: {
-                    Text("Submit Entry")
-                        .fontWeight(.semibold)
-                        .padding()
+                    Button { // action
+                        sessionViewModel.submitSession(comment: description)
+                    } label: {
+                        Text("Submit Entry")
+                            .fontWeight(.semibold)
+                            .padding()
+                    }
                 }
             }
             
             .background(Color("medium"))
+            .foregroundColor(Color(UIColor.systemBackground))
             
             .frame(minHeight: 200, idealHeight: 400, maxHeight: 400)
             .cornerRadius(20)
-
-            .padding(2)
+            
             .overlay(RoundedRectangle(cornerRadius: 20)
-                        .stroke(lineWidth: 5))
+                        .stroke(lineWidth: 2))
             .foregroundColor(.white)
             .padding(30)
         }
