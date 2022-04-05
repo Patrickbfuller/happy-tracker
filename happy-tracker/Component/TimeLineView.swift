991093a569a003b569ca390dc65c0fed94320322
Patrick Fuller <patrickfuller@Patricks-MacBook-Pro.local>
2022-04-05 16:46:29 -0400

    Build out basic timeline view

---
+++ happy-tracker/Component/TimeLineView.swift
@@ -0,0 +1,20 @@
+//
+//  TimeLineView.swift
+//  happy-tracker
+//
+//  Created by Patrick Fuller on 4/5/22.
+//
+
+import SwiftUI
+
+struct TimeLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimeLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeLineView()
+    }
+}
---
+++ happy-tracker/Util/CustomDateFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  CustomDateFormatter.swift
+//  happy-tracker
+//
+//  Created by Patrick Fuller on 4/5/22.
+//
+
+import Foundation
